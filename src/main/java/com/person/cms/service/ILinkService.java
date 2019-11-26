02394a64348f94b1292160daaf4132c6991a0ccd
unknown <2434398632@qq.com>
2019-11-26 19:50:55 +0800

    完成链接管理功能

--- src/main/java/com/person/cms/service/ILinkService.java
+++ src/main/java/com/person/cms/service/ILinkService.java
@@ -3,6 +3,16 @@ package com.person.cms.service;
 import com.person.cms.bean.Link;
 import com.person.cms.exception.CustomerException;
 
+import java.util.List;
+
+
 public interface ILinkService {
-    void addLink(Link link) throws CustomerException;
+    void saveOrUpdateLink(Link link) throws CustomerException;
+
+    void deleteLink(int id) throws CustomerException;
+
+    Link findById(int id) throws CustomerException;
+
+    List<Link> findAll() throws CustomerException;
+
 }
--- src/main/java/com/person/cms/service/impl/LinkServiceImpl.java
+++ src/main/java/com/person/cms/service/impl/LinkServiceImpl.java
@@ -8,15 +8,35 @@ import com.person.cms.util.CodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LinkServiceImpl implements ILinkService {
+
     @Autowired
     private ILinkDao linkDao;
+
     @Override
-    public void addLink(Link link) throws CustomerException {
+    public void saveOrUpdateLink(Link link) throws CustomerException {
         if (link == null) {
             throw new CustomerException(CodeUtil.WORST_CODE,"参数为空");
         }
         linkDao.save(link);
+    }
+
+    @Override
+    public void deleteLink(int id) throws CustomerException {
+        linkDao.deleteById(id);
+    }
+
+    @Override
+    public Link findById(int id) throws CustomerException {
+        return linkDao.queryById(id);
+
+    }
+
+    @Override
+    public List<Link> findAll() throws CustomerException {
+        return linkDao.findAll();
     }
 }
